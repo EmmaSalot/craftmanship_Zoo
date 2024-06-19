e67148288dbbfc8a23d734dbcb9f201a1e49deff
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2024-06-19 17:38:33 +0200

    Ajout d'un premier test (faux)

--- ZooManager.Tests/UnitTest1.cs
+++ ZooManager.Tests/UnitTest1.cs
@@ -3,8 +3,33 @@ namespace ZooManager.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void AjouterAnimal_DoitAjouterAnimal_SiEnclosPeutAccueillir()
     {
+        // Arrange
+        var zoo = new Zoo();
+        var enclos = new Enclos
+        {
+            Id = 1,
+            Nom = "Enclos Grands Félins",
+            Taille = "Grand",
+            EspecesPermises = new List<string> { "Lion" }
+        };
+        zoo.AjouterEnclos(enclos);
 
+        var animal = new Animal
+        {
+            Id = 1,
+            Nom = "Simba",
+            Espece = "Lion",
+            Age = 5,
+            Enclos = enclos
+        };
+
+        // Act
+        zoo.AjouterAnimal(animal);
+
+        // Assert
+        Assert.True(zoo.ObtenirAnimaux().Contains(animal));
+        Assert.True(enclos.Animaux.Contains(animal));
     }
 }
--- ZooManager/zoo/zoo.cs
+++ ZooManager/zoo/zoo.cs
@@ -3,4 +3,21 @@ public class Zoo
     private List<Animal> animaux = new List<Animal>();
     private List<Enclos> enclos = new List<Enclos>();
     private List<Visite> visites = new List<Visite>();
+
+    public void AjouterAnimal(Animal animal)
+    {
+    }
+
+    public void AjouterEnclos(Enclos enclos)
+    {
+    }
+
+    public void AjouterVisite(Visite visite)
+    {
+    }
+
+    public List<Animal> ObtenirAnimaux()
+    {
+        return animaux;
+    }
 }
